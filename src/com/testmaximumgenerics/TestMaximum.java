68c296980b6ce06e7a354329bb6dd0e2aa196527
SRC0747 <RjSrc@0747>
2021-08-16 17:13:15 +0530

    Welcome to Generic problem calculating the Maximum of Integer, Float and String input

---
+++ src/com/testmaximumgenerics/TestMaximum.java
@@ -0,0 +1,7 @@
+package com.testmaximumgenerics;
+
+public class TestMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generic problem calculating the Maximum of Integer, Float and String input");
+    }
+}

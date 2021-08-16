7042d7fd8f11aba50d973014d5321249ed26c7d2
SRC0747 <RjSrc@0747>
2021-08-16 17:29:35 +0530

    added : Calculating the maximum of three floating numbers.

--- src/com/testmaximumgenerics/TestMaximum.java
+++ src/com/testmaximumgenerics/TestMaximum.java
@@ -18,11 +18,33 @@ public class TestMaximum {
         }
         return max;
     }
+    public static void toPrint(Double[] doubleArray) {
+        for (double element : doubleArray) {
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+    public static double compareTo(Double[] doubleArray)
+    {
+        double max = doubleArray[0];
+        for (double element : doubleArray) {
+            if (doubleArray[1] > max)
+                max = doubleArray[1];
+            if (doubleArray[2] > max)
+                max = doubleArray[2];
+        }
+        return max;
+    }
     public static void main(String[] args)
     {
         Integer[] intArray = {30,50,40};
+        Double[] doubleArray = {6.6,7.7,8.8};
+
         TestMaximum.toPrint(intArray);
-        int maximum = TestMaximum.compareTo(intArray);
-        System.out.println("Maximum integer number is:"+maximum);
+        int maximum_int = TestMaximum.compareTo(intArray);
+        System.out.println("Maximum integer number is:"+maximum_int);
+        TestMaximum.toPrint(doubleArray);
+        double maximum_float = TestMaximum.compareTo(doubleArray);
+        System.out.println("Maximum floating number is:"+maximum_float);
     }
 }

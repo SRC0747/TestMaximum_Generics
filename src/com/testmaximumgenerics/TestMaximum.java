5aa855719219742be6c249a147efdfcd3b14bc49
SRC0747 <RjSrc@0747>
2021-08-16 17:20:40 +0530

    added : Calculating the maximum of three integers.

--- src/com/testmaximumgenerics/TestMaximum.java
+++ src/com/testmaximumgenerics/TestMaximum.java
@@ -1,7 +1,28 @@
 package com.testmaximumgenerics;
 
 public class TestMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Generic problem calculating the Maximum of Integer, Float and String input");
+    public static void toPrint(Integer[] inputArray) {
+            for (int element : inputArray) {
+                System.out.println(element);
+            }
+            System.out.println();
+        }
+    public static int compareTo(Integer[] inputArray)
+    {
+        int max = inputArray[0];
+        for (int element : inputArray) {
+            if (inputArray[1] > max)
+                max = inputArray[1];
+            if (inputArray[2] > max)
+                max = inputArray[2];
+        }
+        return max;
+    }
+    public static void main(String[] args)
+    {
+        Integer[] intArray = {30,50,40};
+        TestMaximum.toPrint(intArray);
+        int maximum = TestMaximum.compareTo(intArray);
+        System.out.println("Maximum integer number is:"+maximum);
     }
 }
